167ded5cc846083ce303862909ea93b4c4346d08
Timofey <tima_yakovishin@mail.ru>
2023-04-03 15:56:10 +0300

    [add] add dotenv

--- page_analyzer/configuration/flask_config.py
+++ page_analyzer/configuration/flask_config.py
@@ -1,7 +1,16 @@
 import os
+from dotenv import load_dotenv
 
 
 class Configuration:
     DEBUG = False
     SECRET_KEY = os.urandom(32)
     SESSION_TYPE = 'filesystem'
+
+
+class Psqlconfig:
+    load_dotenv()
+    db_host = os.getenv('PGHOST')
+    db_user = os.getenv('PGUSER')
+    db_password = os.getenv('PGPASSWORD')
+    db_name = os.getenv('PGDATABASE')
