4bc6b2e9a3d3cf0ac1fa90b6302473e81281216b
Timofey <tima_yakovishin@mail.ru>
2023-04-05 18:00:30 +0300

    [fix] shift the app to app file

---
+++ page_analyzer/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_session import Session
+from flask_bootstrap import Bootstrap4
+from .configuration import Config
+from .routes import main
+from .models import db
+from flask_migrate import Migrate
+from .routes import page_not_found
+
+
+def create_app():
+    created_app = Flask(__name__)
+    created_app.register_error_handler(404, page_not_found)
+    created_app.config.from_object(Config)
+    Session(created_app)
+    Bootstrap4(created_app)
+    db.init_app(created_app)
+    Migrate(created_app, db)
+    created_app.register_blueprint(main)
+    return created_app
+
+
+app = create_app()
