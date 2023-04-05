cb3ec26c7be4f86f02440082adebb2e8dbf91711
Timofey <tima_yakovishin@mail.ru>
2023-04-05 18:11:37 +0300

    [feature] add new routs

--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -3,7 +3,8 @@ from flask import (render_template, request,
                    get_flashed_messages,
                    redirect,
                    url_for)
-from .link_validator import validate_link
+from .link_validator import Validator
+from .models import db, Urls
 
 main = Blueprint("main", __name__)
 
@@ -16,18 +17,51 @@ def main_page():
 
 @main.route('/urls', methods=["GET"])
 def urls():
-    return render_template('urls.html')
+    data = Urls.query.all()
+    return render_template('urls.html', data=data[::-1])
+
+
+@main.route('/urls/<id>')
+def url_page(id):
+    data = Urls.query.filter_by(id=id)
+    message = get_flashed_messages(with_categories=True)
+    if list(data):
+        return render_template('url.html', data=data, messages=message)
+    # return redirect(url_for('main.page_not_found'))
 
 
 @main.route('/urls', methods=["POST"])
 def get_url():
     url = request.form['url']
-    validation = validate_link(url)
+    validator = Validator(url)
+    validation = validator.validate_link()
     if not url:
         flash("URL обязателен", "failed")
     if not validation:
         flash("Некорректный URL", "failed")
     if not url or not validation:
         return redirect(url_for('main.main_page'))
+
+    data = Urls.query.all()
+    val = validator.validate_unique_link(data)
+
+    if val:
+        flash("Страница уже существует", "info")
+        return redirect(url_for('main.url_page', id=val[0].id))
+
+    db.session.add(Urls(name=validation))
+
+    db.session.commit()
+    id = 1 if not data else data[-1].id + 1
     flash("Страница успешно добавлена", "success")
-    return redirect(url_for('main.urls'))
+    return redirect(url_for('main.url_page', id=id))
+
+
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template("unknown_page.html"), 404
+
+
+@main.errorhandler(500)
+def internal_server_error(e):
+    return render_template("unknown_page.html"), 500
