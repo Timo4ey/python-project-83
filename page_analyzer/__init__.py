7f6b8688460dc297abad14607045c3876d52dee2
Timofey <tima_yakovishin@mail.ru>
2023-04-05 18:06:58 +0300

    [refactor] shift app to app.py file

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,14 +1,3 @@
-from flask import Flask
-from flask_session import Session
-from flask_bootstrap import Bootstrap4
-from .configuration import Config
-from .routes import main
+from .app import app
 
-
-def app():
-    created_app = Flask(__name__)
-    created_app.config.from_object(Config)
-    Session(created_app)
-    Bootstrap4(created_app)
-    created_app.register_blueprint(main)
-    return created_app
+__all__ = ["app"]
