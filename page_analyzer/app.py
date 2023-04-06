8bbc3251cff50637b13ecc8801661288d031aab5
Timofey <tima_yakovishin@mail.ru>
2023-04-06 14:51:56 +0300

    [refacot] add if name statement

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,16 +1,16 @@
 from flask import Flask
 from flask_session import Session
 from flask_bootstrap import Bootstrap4
-from .configuration import Config
-from .routes import main
-from .models import db
-from .routes import page_not_found
+from page_analyzer.config import DevConfig, ProdConfig
+from page_analyzer.routes import main
+from page_analyzer.models import db
+from page_analyzer.routes import page_not_found
 
 
 def create_app():
     created_app = Flask(__name__)
     created_app.register_error_handler(404, page_not_found)
-    created_app.config.from_object(Config)
+    created_app.config.from_object(ProdConfig)
     Session(created_app)
     Bootstrap4(created_app)
     db.init_app(created_app)
@@ -19,3 +19,7 @@ def create_app():
 
 
 app = create_app()
+
+if __name__ == "__main__":
+    app.config.from_object(DevConfig)
+    app.run()
