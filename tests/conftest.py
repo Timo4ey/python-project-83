456520267c8757a53ab799b78c20519e354b5cfb
Timofey <tima_yakovishin@mail.ru>
2023-04-06 16:27:07 +0300

    [fix] add in config SQLALCHEMY_BINDS = {users: PsqlConfig.DATABASE_URL}

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
 import pytest
 from page_analyzer import app as create_app
-from page_analyzer.config import PsqlConfig
 
 
 @pytest.fixture()
 def application():
     app = create_app
-    app.config.setdefault("SQLALCHEMY_DATABASE_URI", PsqlConfig.DATABASE_URL)
-    app.config.setdefault("SQLALCHEMY_BINDS", None)
     yield app
 
 
