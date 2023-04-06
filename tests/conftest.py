1bfa172aaace56e00b1af19ba6d0dce257c662cd
Timofey <tima_yakovishin@mail.ru>
2023-04-06 16:19:57 +0300

    [fix] add in conftest app.config.setdefault(SQLALCHEMY_DATABASE_URI, PsqlConfig.DATABASE_URL)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from page_analyzer import app as create_app
-
+from page_analyzer.config import PsqlConfig
 
 
 @pytest.fixture()
 def application():
     app = create_app
+    app.config.setdefault("SQLALCHEMY_DATABASE_URI", PsqlConfig.DATABASE_URL)
     app.config.setdefault("SQLALCHEMY_BINDS", None)
     yield app
 
