64110f4338cf271d8668356d031141641106b02e
Timofey <tima_yakovishin@mail.ru>
2023-04-06 18:19:29 +0300

    [fix] update lint

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_session import Session
 from flask_bootstrap import Bootstrap4
-from page_analyzer.config import DevConfig, ProdConfig, PsqlConfig
+from page_analyzer.config import DevConfig, ProdConfig
 from page_analyzer.routes import main
 from page_analyzer.models import db
 from page_analyzer.routes import page_not_found
