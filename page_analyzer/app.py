0106a2ba119e966d503987c8157ef866eca23348
Timofey <tima_yakovishin@mail.ru>
2023-04-06 15:31:30 +0300

    [fix] shift db.init higher but dont completely sure it will help

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,18 +5,17 @@ from page_analyzer.config import DevConfig, ProdConfig
 from page_analyzer.routes import main
 from page_analyzer.models import db
 from page_analyzer.routes import page_not_found
-from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
     created_app = Flask(__name__)
+
     created_app.register_error_handler(404, page_not_found)
     created_app.config.from_object(ProdConfig)
+    db.init_app(created_app)
+    created_app.register_blueprint(main)
     Session(created_app)
     Bootstrap4(created_app)
-    db.init_app(created_app)
-    SQLAlchemy(created_app)
-    created_app.register_blueprint(main)
     return created_app
 
 
