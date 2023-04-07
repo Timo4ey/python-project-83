e2bfa7c83a581fa298106daa2982f5748250f885
Timofey <tima_yakovishin@mail.ru>
2023-04-07 22:34:32 +0300

    [fix] fix url for engine

--- page_analyzer/config/config.py
+++ page_analyzer/config/config.py
@@ -19,8 +19,7 @@ class Config:
     SESSION_COOKIE_NAME = 'session'
     TEMPLATES_FOLDER = 'templates'
     STATIC_FOLDER = 'static'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_BINDS = {'database': os.getenv('DATABASE_URL')}
+    SQLALCHEMY_DATABASE_URI = PsqlConfig.DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
