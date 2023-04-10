d1a647d8b5216a9ea7374a85b0b15f53b5811302
Timofey <tima_yakovishin@mail.ru>
2023-04-10 13:15:12 +0300

    [fix] change conftests scope to function

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,17 @@ import pytest
 from page_analyzer import app as create_app
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def application():
     app = create_app
     yield app
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def client(application):
     return application.test_client()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def runner(application):
     return application.test_cli_runner()
