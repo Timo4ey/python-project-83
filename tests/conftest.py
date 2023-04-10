e88929e709dcbcb8d7859346aa9f5b2ccdd7be8c
Timofey <tima_yakovishin@mail.ru>
2023-04-10 13:11:57 +0300

    [fix] change conftests scope to session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,17 @@ import pytest
 from page_analyzer import app as create_app
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def application():
     app = create_app
     yield app
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def client(application):
     return application.test_client()
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def runner(application):
     return application.test_cli_runner()
