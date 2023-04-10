7b617cd3df1fba098732c3051f96661028cccf0b
Timofey <tima_yakovishin@mail.ru>
2023-04-10 13:35:00 +0300

    [fix] fix encoding in pg_dump

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,17 @@ import pytest
 from page_analyzer import app as create_app
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def application():
     app = create_app
     yield app
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def client(application):
     return application.test_client()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
 def runner(application):
     return application.test_cli_runner()
