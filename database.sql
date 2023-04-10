278b6f71951fff41b7d5097773f5f9aac5a8eb6f
Timofey <tima_yakovishin@mail.ru>
2023-04-10 16:48:52 +0300

    [fix] mix dump_db and dump_table

--- database.sql
+++ database.sql
@@ -82,9 +82,9 @@ ALTER TABLE public.urls ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 -- Data for Name: url_checks; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES (1, 'https://ru.hexlet.io', '2023-04-10 12:39:15.031636');
-INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES (2, 'https://stepik.org', '2023-04-10 12:39:37.138841');
-INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES (3, 'https://docs.sqlalchemy.org', '2023-04-10 12:40:11.089756');
+INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES ('https://ru.hexlet.io', '2023-04-10 12:39:15.031636');
+INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES ('https://stepik.org', '2023-04-10 12:39:37.138841');
+INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES ( 'https://docs.sqlalchemy.org', '2023-04-10 12:40:11.089756');
 
 
 
@@ -92,15 +92,15 @@ INSERT INTO public.urls (id, name, created_at) OVERRIDING SYSTEM VALUE VALUES (3
 -- Name: url_checks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (1, 1, 200, 'Онлайн-школа программирования, за выпускниками которой охотятся компании
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES ( 1, 200, 'Онлайн-школа программирования, за выпускниками которой охотятся компании
 ', 'Хекслет — больше чем школа программирования. Онлайн курсы, сообщество программистов', 'Живое онлайн сообщество программистов и разработчиков на JS, Python, Java, PHP, Ruby. Авторские программы обучения с практикой и готовыми проектами в резюме. Помощь в трудоустройстве после успешного окончания обучения', '2023-04-10 12:39:04.535051');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (2, 2, 200, '
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES ( 2, 200, '
 
         Скачай курс в приложении
 
     ', '
 ', 'Promoting efficient learning by developing innovative educational technologies. Create an interactive lesson and employ automated grading software.', '2023-04-10 12:39:04.535051');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (3, 3, 200, 'SQLAlchemy 2.0 Documentation', '
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES ( 3, 200, 'SQLAlchemy 2.0 Documentation', '
 
 
 
@@ -110,7 +110,7 @@ INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, 
 
 
     ', 'None', '2023-04-10 12:39:04.535051');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (4, 3, 200, 'SQLAlchemy 2.0 Documentation', '
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (3, 200, 'SQLAlchemy 2.0 Documentation', '
 
 
 
@@ -120,7 +120,7 @@ INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, 
 
 
     ', 'None', '2023-04-10 12:44:09.363142');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (5, 3, 200, 'SQLAlchemy 2.0 Documentation', '
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (3, 200, 'SQLAlchemy 2.0 Documentation', '
 
 
 
@@ -130,9 +130,9 @@ INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, 
 
 
     ', 'None', '2023-04-10 12:52:04.573461');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (6, 1, 200, 'Онлайн-школа программирования, за выпускниками которой охотятся компании
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (1, 200, 'Онлайн-школа программирования, за выпускниками которой охотятся компании
 ', 'Хекслет — больше чем школа программирования. Онлайн курсы, сообщество программистов', 'Живое онлайн сообщество программистов и разработчиков на JS, Python, Java, PHP, Ruby. Авторские программы обучения с практикой и готовыми проектами в резюме. Помощь в трудоустройстве после успешного окончания обучения', '2023-04-10 12:52:04.824145');
-INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES (7, 3, 200, 'SQLAlchemy 2.0 Documentation', '
+INSERT INTO public.url_checks (id, url_id, status_code, h1, title, description, created_at) OVERRIDING SYSTEM VALUE VALUES ( 3, 200, 'SQLAlchemy 2.0 Documentation', '
 
 
 
