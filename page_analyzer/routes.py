f82cbfaf1c7cb09e85d1addee29fd1c3e5bd291d
Timofey <tima_yakovishin@mail.ru>
2023-04-11 13:42:39 +0300

    [refactor] udpate html. delete unused

--- page_analyzer/routes.py
+++ page_analyzer/routes.py
@@ -76,9 +76,5 @@ def checker_page(id):
 
 @main.errorhandler(404)
 def page_not_found(e):
-    return render_template("unknown_page.html"), 404
+    return render_template("404.html"), 404
 
-
-@main.errorhandler(500)
-def internal_server_error(e):
-    return render_template("unknown_page.html"), 500
